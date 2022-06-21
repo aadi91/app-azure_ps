e46bd1ee7a3eb60208b557401ee7eb737229589e
aadi91 <destiny.aditya91@gmail.com>
2022-06-21 08:15:49 -0500

    GetCosmosDBData PS Script Enhancements

--- mainScripts/getAzureCosmosDbData.ps1
+++ mainScripts/getAzureCosmosDbData.ps1
@@ -1,20 +1,17 @@
-connect-azAccount
+Connect-AzAccount
 $date = Get-Date -UFormat("%m-%d-%yT%H-%M-%S")
 $currentDir = $(Get-Location).Path
-$oFile = "$($currentDir)\List_Of_All_Azure_Resources_$($date).csv"
+$targetFilePath = "$($currentDir)\Azure_CosmosDB_Usage_Report-$($date).csv"
+#$oFilePath = "U:\Azure_CosmosDB_Usage_Report-$($date).csv"
 
-if (Test-Path $oFile) {
-    Remove-Item $oFile -Force
-}
- 
-"COSMOS_DB_NAME,SUBSCRIPTION_NAME,RESOURCE_GROUP_NAME,DEPARTMENT,COST_CENTER,ENV_TYPE,MATRIC_MAX" | Out-File $oFile -Append -Encoding ascii
+"COSMOS_DB_NAME,SUBSCRIPTION_NAME,RESOURCE_GROUP_NAME,DEPARTMENT,COST_CENTER,ENV_TYPE,METRIC_MAX,MONTHLY_TOTAL_ROUND_COST$" | Out-File $targetFilePath -Append -Encoding ascii
  
 Get-AzSubscription | ForEach-Object {
     $subscriptionId = $_.Id
     $subscriptionName = $_.Name
-    #$subscriptionTags = $_.Tags
-    #$TenentID = $_.TenantId
-    #$tagcount = $_.Tags.Count
+    $subscriptionTags = $_.Tags
+    $TenentID = $_.TenantId
+    $tagcount = $_.Tags.Count
     Set-AzContext -SubscriptionId $subscriptionId
     Get-AzResourceGroup | ForEach-Object {
         $resourceGroupName = $_.ResourceGroupName
@@ -24,14 +21,26 @@ Get-AzSubscription | ForEach-Object {
             $Department = $_.Tags.Department
             $CostCentre = $_.Tags.CostCenter
             $EnvType = $_.Tags.EnvType
-            #$name = (Get-AzResource -ResourceId $cdbid).Name
+            $name = (Get-AzResource -ResourceId $cdbid).Name
             $metricdata = Get-AzMetric -ResourceId $cdbid -TimeGrain 00:05:00 -MetricName "NormalizedRUConsumption" -AggregationType Maximum -WarningAction Ignore -StartTime (Get-Date).adddays(-30) -EndTime (Get-Date) -DetailedOutput
             $metricdata | ForEach-Object {
                 $metric_used = $_.Data.Maximum | measure -Maximum
                 $metric_maximum = $metric_used.Maximum
-                "$cdbName,$subscriptionName,$resourceGroupName,$Department,$CostCentre,$EnvType,$metric_maximum" | Out-File $oFile -Append -Encoding ascii
+                $Consumption = Get-AzConsumptionUsageDetail -StartDate (Get-Date).adddays(-30) -EndDate (Get-Date) -InstanceID $cdbid
+                $Costs = $Consumption.PretaxCost
+                Write-Output $Costs
+                $MonthlyCostTotal = 0
+                foreach ($Cost in $Costs) { $MonthlyCostTotal += $Cost }
+                Write-Output $Cost
+                Write-Output $MonthlyCostTotal
+                $MonthlyCostTotalRound = [math]::Round($MonthlyCostTotal)
+                "$cdbName,$subscriptionName,$resourceGroupName,$Department,$CostCentre,$EnvType,$metric_maximum,$MonthlyCostTotalRound" | Out-File $targetFilePath -Append -Encoding ascii
             }
         }
     }
 }
-Write-Host "script executed successfully" -ForegroundColor Green
+
+$StartTime = get-date 
+start-sleep -Seconds 5 
+$RunTime = New-TimeSpan -Start $StartTime -End (get-date) 
+Write-Host "Completed. Script took "$($RunTime.Hours) "hours. Check report at" $targetFilePath -ForegroundColor Green
